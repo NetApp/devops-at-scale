acf284051221a7e06025a15596470562475779ad
Devops Admin <garchana@netapp.com>
2019-05-31 16:50:46 -0700

    - Remove build-at-scale directory - Remove web_service directory

--- web_service/run.py
+++
@@ -1,6 +0,0 @@
-import os
-from web_service import create_app
-
-PORT = int(os.getenv('PORT', '80'))
-app = create_app()
-app.run(host='0.0.0.0', port=PORT, threaded=True)
--- web_service/web_service/ontap/ontap_apis/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["cwontap_apis"]
