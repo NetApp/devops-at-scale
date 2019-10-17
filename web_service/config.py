22d56f5ace1c03a5a1b6e8c3769f0e97ba133065
Laurent Nicolas <laurentn@netapp.com>
2019-10-17 15:47:12 -0400

    make pipeline code more readable, add missing config variables

--- web_service/config.py
+++ web_service/config.py
@@ -15,26 +15,39 @@ class BaseConfig:
     ONTAP_AGGR_NAME = os.getenv('ONTAP_AGGR_NAME', 'olb03_sat1')
     ONTAP_DATA_IP = os.getenv('ONTAP_DATA_IP', '10.193.8.78')
 
-    # SCM
-    SCM_SERVICE_NAME = os.getenv('SCM_SERVICE_NAME', 'devops-at-scale-gitlab')
-    SCM_PVC_NAME = os.getenv('SCM_PVC_NAME', 'devops-at-scale-gitlab-pvc')
-
-    # Jenkins
-    JENKINS_SERVICE_NAME = os.getenv('JENKINS_SERVICE_NAME', 'devops-at-scale-jenkins')
-
-    # Database CouchDB
-    DATABASE_SERVICE_NAME = os.getenv('DATABASE_SERVICE_NAME', 'devops-at-scale-couchdb')
-    # default username, password
+    # default username, password for couchdb
     DATABASE_USER = os.getenv('DATABASE_USER', 'admin')
     DATABASE_PASS = os.getenv('DATABASE_PASS', 'admin')
-    DATABASE_NAME = os.getenv('DATABASE_NAME', 'devops_at_scale')
+    DATABASE_NAME = os.getenv('DATABASE_NAME', 'devops-at-scale')
+
+    # The following values are set during Helm deployment
+
+    # SCM
+    SCM_SERVICE_NAME = os.getenv('SCM_SERVICE_NAME')
+    SCM_PVC_NAME = os.getenv('SCM_PVC_NAME')
+    SCM_TYPE = os.getenv('SCM_TYPE')
+
+    # Jenkins
+    JENKINS_SERVICE_NAME = os.getenv('JENKINS_SERVICE_NAME')
+    JENKINS_PVC_NAME = os.getenv('JENKINS_PVC_NAME')
+
+    # Database CouchDB
+    DATABASE_SERVICE_NAME = os.getenv('DATABASE_SERVICE_NAME')
+    DATABASE_PVC_NAME = os.getenv('DATABASE_PVC_NAME')
 
     # Artifactory
-    REGISTRY_SERVICE_NAME = os.getenv('DATABASE_SERVICE_NAME', 'devops-at-scale-artifactory')
-    REGISTRY_TYPE = os.getenv('REGISTRY_TYPE', 'artifactory')
+    REGISTRY_SERVICE_NAME = os.getenv('REGISTRY_SERVICE_NAME')
+    REGISTRY_PVC_NAME = os.getenv('REGISTRY_PVC_NAME')
+    REGISTRY_TYPE = os.getenv('REGISTRY_TYPE')
 
     # Webservice
-    WEB_SERVICE_NAME = os.getenv('WEB_SERVICE_NAME', 'devops-at-scale-webservice')
+    WEB_SERVICE_NAME = os.getenv('WEB_SERVICE_NAME')
+    WEB_PVC_NAME = os.getenv('WEB_PVC_NAME')
+
+    # Kube specs
+    KUBE_NAMESPACE = os.getenv('KUBE_NAMESPACE')
+    STORAGE_CLASS = os.getenv('STORAGE_CLASS')
+    SERVICE_TYPE = os.getenv('SERVICE_TYPE')
 
 
 class TestingConfig(BaseConfig):
